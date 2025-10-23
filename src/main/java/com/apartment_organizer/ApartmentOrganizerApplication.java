a6c3b4477df02d05b6ae94dbb612480656105b0f
Nikolai0902 <kbus94@yandex.ru>
2025-10-23 20:24:14 +0300

    add Readme.md

---
+++ src/main/java/com/apartment_organizer/ApartmentOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.apartment_organizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apartment_organizer/ApartmentOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apartment_organizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
